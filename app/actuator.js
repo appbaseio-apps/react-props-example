c7319ad509c3866903ebd996863376b527b52b39
shahsaurabh0605 <shahsaurabh0103@gmail.com>
2017-05-23 13:57:28 +0530

    master branch routing changes

--- app/actuator.js
+++ app/actuator.js
@@ -28,15 +28,15 @@ class Actuator extends Component {
   }
 
 	render() {
-		if(this.props.path == '/react-props-example/' || this.props.path =='/react-props-example/1'){
+		if(this.props.path == '/' || this.props.path =='/1'){
 					this.updateCode(`{"and": "LocationSensor"}`)
 					// this.state = ({code: `{"and": "LocationSensor"}`})
 		}
-		else if(this.props.path == '/react-props-example/2'){
+		else if(this.props.path == '/2'){
 					this.updateCode(`{"and": "CitySensor"}`)
 					// this.state = ({code: `{"and": "CitySensor"}`})
 		}
-		else if(this.props.path == '/react-props-example/3'){
+		else if(this.props.path == '/3'){
 					this.updateCode(`{"and": "GuestSensor"}`)
 					// this.state = ({code: `{"and": "GuestSensor"}`})
 		}
